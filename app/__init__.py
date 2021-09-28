c0f6efd455fe8e24f11ff564036e16c09c87950b
kilonzijnr <vkilonzi05@gmail.com>
2021-09-28 12:11:13 +0300

    initialised app

---
+++ app/__init__.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from .config import config_options
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_uploads import IMAGES, UploadSet, configure_uploads
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "auth.signin"
+photos = UploadSet("photos", IMAGES)
+
+
+def create_app(config_name):
+
+    app = Flask(__name__)
+
+    app.config.from_object(config_options[config_name])
+
+    from .auth import auth as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    configure_uploads(app, photos)
+    mail.init_app(app)
+
+   
+    return app
